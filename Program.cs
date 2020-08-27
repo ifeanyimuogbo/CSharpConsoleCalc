090b3782f51c9a538579eae930773c0b26bdbe5a
Kingsley <doc3coder@gmail.com>
2020-08-27 03:30:36 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variables2 {
+    class Program {
+        static void Main (string[] args) {
+            int firstNumber;
+            int secondNumber;
+            int result;
+            
+            Console.Write ("Please enter the first number");
+            firstNumber = int.Parse(Console.ReadLine ());
+
+            Console.Write ("Please, Enter the second number: ");
+            secondNumber = int.Parse(Console.ReadLine ());
+            result = firstNumber / secondNumber;
+            Console.WriteLine ("The answer is " + result);
+
+        }
+    }
+}
